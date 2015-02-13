4b2c8d4378ab1e9e0ba6817b224a42ae358a14f7
JJSeger <jjsbowhunter@yahoo.com>
2015-02-13 08:44:47 -0500

    debugging and adding comments
    debugging and adding comments

--- wk-2/goal3_assign_debug_v2/js/search.js
+++ wk-2/goal3_assign_debug_v2/js/search.js
@@ -8,19 +8,19 @@
 	;
 	
 	// Validates search query
-	var validqte == function(query){
+	var validqte = function(query){                                 //removed one of the =
 		
 		// Trim whitespace from start and end of search query
-		while(query.charAt(0) = " "){
+		while(query.charAt(0) === " "){                                     //replaced the = with ===
 			query = query.substring(1, query.length);
 		};
-		while(query.charAt(query.length-1) === ""){
-			query = query.substring(0, query.length-1);
-		;
+		while(query.charAt(query.length-1) === "") {
+            query = query.substring(0, query.length - 1);
+        ;
 		
 		// Check search length, must have 3 characters
 		if(query.length < 3){
-			alert("Your search query is too small, try again.);
+			alert("Your search query is too small, try again.");      //added quote to string
 			
 			// (DO NOT FIX THE LINE DIRECTLY BELOW)
 			searchInput.focus();
@@ -34,32 +34,32 @@
 	var search = function(query)
 		
 		// split the user's search query string into an array
-		var queryArray = query.join(" ");
+		queryArray = query.join(" ");                                   //removed the var
 		
 		// array to store matched results from database.js
 		var results = [];
 
 		// loop through each index of db array
-		for(var i=0, j=db.length; i<j; i++){
-		
-			// each db[i] is a single video item, each title ends with a pipe "|"
-			// save a lowercase variable of the video title
-			var dbTitleEnd = db[i].indexOf('|');
-			var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
-			
-			// loop through the user's search query words
-			// save a lowercase variable of the search keyword
-			for(var ii=0, jj=queryArray.length; ii<jj; ii++){
-				var qitem = queryArray[ii].tolowercase();
-				
-				// is the keyword anywhere in the video title?
-				// If a match is found, push full db[i] into results array
-				var compare = dbitem.indexOf(qitem);
-				if(compare !== -1){
-					results.push(db[i]);
-				};
-			;
-		;
+		for(var i=0, j=db.length; i<j; i++) {
+
+            // each db[i] is a single video item, each title ends with a pipe "|"
+            // save a lowercase variable of the video title
+            var dbTitleEnd = db[i].indexOf('|');
+            var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
+
+            // loop through the user's search query words
+            // save a lowercase variable of the search keyword
+            for (var ii = 0, jj = queryArray.length; ii < jj; ii++) {
+                var qitem = queryArray[ii].tolowercase();
+
+                // is the keyword anywhere in the video title?
+                // If a match is found, push full db[i] into results array
+                var compare = dbitem.indexOf(qitem);
+                if (compare !== -1) {
+                    results.push(db[i]);
+                };
+            };                                    //added closing curly brace
+        };                                      //added closing curly brace
 		
 		results.sort();
 		
@@ -81,13 +81,13 @@
 	};
 	
 	// Put matches into page as paragraphs with anchors
-	var showMatches = function(results){
-		
-		// THE NEXT 4 LINES ARE CORRECT.
-		var html = '<p>Results</p>', 
-			title, 
-			url
-		;
+	var showMatches = function(results) {
+
+        // THE NEXT 4 LINES ARE CORRECT.
+        var html = '<p>Results</p>',
+            title,
+            url
+    ;
 		
 		// loop through all the results search() function
 		for(var i=0, j=results.length; i<j; i++){
@@ -108,13 +108,13 @@
 	
 	// The onsubmit event will be reviewed in upcoming Course Material.
 	// THE LINE DIRECTLY BELOW IS CORRECT
-	document.forms[0].onsubmit = function(){
-		var query = searchInput.value;
-		validqte(query);
+	document.forms[0].onsubmit = function() {
+        var query = searchInput.value;
+        validqte(query);
 
         // return false is needed for most events - this will be reviewed in upcoming course material
         // THE LINE DIRECTLY BELOW IS CORRECT
-		return false;
-	;
+        return false;
+    };                                              //added closing curly brace
 
 })();
