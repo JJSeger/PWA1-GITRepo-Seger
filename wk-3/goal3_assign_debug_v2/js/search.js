b555ecbd93b2ddb96c48820e32b247f5311465ef
JJSeger <jjsbowhunter@yahoo.com>
2015-02-20 15:23:39 -0500

    replaced single equal sign with triple equal on line 74
    replaced single equal sign with triple equal

--- wk-3/goal3_assign_debug_v2/js/search.js
+++ wk-3/goal3_assign_debug_v2/js/search.js
@@ -51,12 +51,12 @@
             // each db[i] is a single video item, each title ends with a pipe "|"
             // save a lowercase variable of the video title
             var dbTitleEnd = db[i].indexOf('|');
-            var dbitem = db[i].toLowerCase().substring(0, dbTitleEnd);          // fixed a syntax problem with the toLowerCase method
+            var dbitem = db[i].toLowerCase().substring(0, dbTitleEnd);         // fixed a syntax problem with the toLowerCase method(camel case)
 
             // loop through the user's search query words
             // save a lowercase variable of the search keyword
             for (var ii = 0, jj = queryArray.length; ii < jj; ii++) {
-                var qitem = queryArray[ii].toLowerCase();                        // fixed a syntax problem with the toLowerCase method
+                var qitem = queryArray[ii].toLowerCase();                        // fixed a syntax problem with the toLowerCase method(camel case)
 
                 // is the keyword anywhere in the video title?
                 // If a match is found, push full db[i] into results array
@@ -71,7 +71,7 @@
         results.sort();
 
         // Check that matches were found, and run output functions
-        if(results.length = 0){
+        if(results.length === 0){                                       //replaced single equal sign with triple equal
             noMatch();
         }else{
             showMatches(results);
